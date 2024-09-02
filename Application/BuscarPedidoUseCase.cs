99909e4351e426ecb529507e7132af334ac05137
AlexisCesar <alexis.cesar2017@outlook.com>
2024-09-02 15:52:57 -0300

    Order Pedidos by status and time

--- Application/BuscarPedidoUseCase.cs
+++ Application/BuscarPedidoUseCase.cs
@@ -9,6 +9,10 @@ namespace ControleDePedidos.UseCases
     public class BuscarPedidoUseCase : IBuscarPedidoUseCase
     {
         private readonly IPedidoPersistencePort PedidoPersistencePort;
+        private Dictionary<string, int> PrioridadeStatus = Enum.GetValues(typeof(Status))
+                                                                .Cast<Status>()
+                                                                .Select((status, index) => new { status, index })
+                                                                .ToDictionary(x => x.status.ToString(), x => x.index);
 
         public BuscarPedidoUseCase(IPedidoPersistencePort pedidoPersistencePort)
         {
@@ -18,17 +22,23 @@ namespace ControleDePedidos.UseCases
         public async Task<List<PedidoDto>> GetAllPedidosAsync()
         {
             var pedidos = await PedidoPersistencePort.GetAllPedidosNaoFinalizadosAsync();
-            var pedidosDto = pedidos.Select(x => x.ToPedidoDto()).ToList();
+            var pedidosDto = pedidos.Select(x => x.ToPedidoDto());
 
-            return pedidosDto;
+            return OrdernarPedidosPorStatusEHora(pedidosDto).ToList();
         }
 
         public async Task<List<PedidoDto>> GetAllPedidosByStatusAsync(Status status)
         {
             var acompanhamentos = await PedidoPersistencePort.GetAllPedidosByStatusAsync(status);
-            var acompanhamentosDto = acompanhamentos.Select(x => x.ToPedidoDto()).ToList();
+            var acompanhamentosDto = acompanhamentos.Select(x => x.ToPedidoDto());
 
-            return acompanhamentosDto;
+            return acompanhamentosDto.OrderBy(x => x.HorarioRecebimento).ToList();
+        }
+
+        private IEnumerable<PedidoDto> OrdernarPedidosPorStatusEHora(IEnumerable<PedidoDto> pedidos)
+        {
+            return pedidos.OrderBy(p => PrioridadeStatus[p.StatusPedido!])
+                            .ThenBy(p => p.HorarioRecebimento);
         }
     }
 }
