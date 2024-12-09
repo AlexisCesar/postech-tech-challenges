b838f9afa429f78ac8ef3ef53cde2b558d18b8a1
Alexis Cesar <alexis.cesar2017@outlook.com>
2024-12-08 23:27:12 -0300

    Add indexes to Produto and Acompanhamento tables

---
+++ Infrastructure/Migrations/20241209021710_AddProdutoAndPedidoIndexes.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ControleDePedidos.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProdutoAndPedidoIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Categoria",
+                table: "Produto",
+                column: "Categoria");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Status",
+                table: "Acompanhamento",
+                column: "Status");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Categoria",
+                table: "Produto");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Status",
+                table: "Acompanhamento");
+        }
+    }
+}
